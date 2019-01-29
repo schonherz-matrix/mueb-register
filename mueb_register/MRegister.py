5d793b397d2454b0d6e37c919b39bfe8564eee69
Zsombor Bodnár <zsombor.bodnar@kszk.bme.hu>
2019-01-29 12:21:40 +0100

    Change to socketserver, Update image output

--- mueb_register/MRegister.py
+++ mueb_register/MRegister.py
@@ -1,82 +1,90 @@
 import qrcode
-import socket
 import sqlite3
+import socketserver
+from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 
-def main():
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
 
-    conn = sqlite3.connect("MUEB_register.db")
-    c = conn.cursor()
+conn = sqlite3.connect("MUEB_register.db")
+c = conn.cursor()
 
-    # create mueb table
-    try:
-        c.execute('''
-        CREATE TABLE mueb (
-            mueb_id INTEGER     PRIMARY KEY,
-            mac     STRING (17) UNIQUE
-                                NOT NULL
-        );
-        ''')
-    except:
-        pass
+# create mueb table
+try:
+    c.execute('''
+    CREATE TABLE mueb (
+        mueb_id INTEGER     PRIMARY KEY,
+        mac     STRING (17) UNIQUE
+                            NOT NULL
+    );
+    ''')
+except:
+    pass
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('', 67))
+print("Listening for MUEB DHCP discover")
+print("{:<8} {:<17} {:<5}".format("scan", "MAC", "MUEB id"))
 
-    prev_MAC = None
-    id = 0
+prev_MAC = None
 
-    print("Listening for MUEB DHCP discover")
-    print("{:<5} {:<20} {:<5}".format("scan", "MAC", "MUEB id"))
 
-    while 1:
-        # wait for dhcp packet
-        data = s.recv(1024)
-        tmp = list(data[28:34])
-        if(hex(tmp[0]) != '0x54' or hex(tmp[1]) != '0x10' or hex(tmp[2]) != '0xec'):
-            continue
+class UDPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        global prev_MAC
+        data = self.request[0].strip()
+        CHADDR = list(data[28:34])  # CHWADDR
+        if(hex(CHADDR[0]) != '0x54' or hex(CHADDR[1]) != '0x10' or hex(CHADDR[2]) != '0xec'):
+            return
 
-        MAC = ':'.join(["{0:02X}".format(i) for i in list(data[28:34])])
+        MAC = ':'.join(["{0:02X}".format(i) for i in CHADDR])
 
-        # save to DB
-        c.execute("INSERT OR IGNORE INTO mueb(mac) VALUES('{}')".format(MAC))
-        conn.commit()
-
-        c.execute("SELECT mueb_id FROM mueb WHERE mac='{}'".format(MAC))
-        mueb_id = c.fetchall()[0][0]
-
-        print("{:<5} {:<20} {:<5}".format(id, MAC, mueb_id))
-        id += 1
-
-        # handle multiple requests
+        # handle multiple requests from the same device
         if prev_MAC != MAC:
+            # save to DB
+            c.execute("INSERT OR IGNORE INTO mueb(mac) VALUES('{}')".format(MAC))
+            conn.commit()
+
+            c.execute("SELECT mueb_id FROM mueb WHERE mac='{}'".format(MAC))
+            mueb_id = c.fetchall()[0][0]
+
+            print("{:<8} {:<17} {:<5}".format(
+                datetime.now().strftime('%H:%M:%S'), MAC, mueb_id))
             qr.add_data(MAC)
             qr.make(fit=True)
 
             img = qr.make_image(fill_color="black", back_color="white")
-            width, height = img.size
+            W, H = img.size
 
-            finalImg = Image.new('RGBA', (width, height), 'white')
+            font = ImageFont.truetype("arial.ttf", 30)
+            w, h = font.getsize(MAC)
+            caption = MAC+"\n\n{}".format(str(mueb_id).center(32))
+
+            finalImg = Image.new(
+                'RGBA', (W+w+qr.border*qr.box_size, H), 'white')
             finalImg.paste(img)
 
-            caption = MAC+"/{}".format(mueb_id)
-            font = ImageFont.truetype("arial.ttf", 15)
-            w, h = font.getsize(caption)
-
             draw = ImageDraw.Draw(finalImg)
-            draw.text((round((width-w)/2), round(height-h-10)),
-                    caption, "black", font)
+            draw.text((W, (H-(qr.border*qr.box_size))/2),
+                      caption, "black", font)
 
             finalImg.show()
             finalImg.save("{}.png".format(mueb_id))
             qr.clear()
+
         prev_MAC = MAC
+
+
+def main():
+    with socketserver.UDPServer(('', 67), UDPHandler) as server:
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            conn.close()
+
 
 if __name__ == '__main__':
     main()
